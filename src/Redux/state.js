ee3ca52ab76b606bda51fa6dbdc680bd1a33ec0c
Ilya Shved <ilia83ivanovich@gmail.com>
2020-12-08 22:19:38 +0500

    37 this_bind

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,57 +1,66 @@
-let rerenderEntireTree = () => {
-    console.log('!!!');
-}
-let state = {
 
-    profilePage: {
-        postData: [
-            { id: 0, message: 'ну че кого???', likeCount: 12 },
-            { id: 0, message: 'та пока не понятно', likeCount: 34 },
-            { id: 0, message: 'учим react', likeCount: 1 },
-            { id: 0, message: 'тра-та-та', likeCount: 74 },
-        ],
-        newPostText: 'введите текст',
+let store = {
+
+    _state: {
+
+        profilePage: {
+            postData: [
+                { id: 0, message: 'ну че кого???', likeCount: 12 },
+                { id: 0, message: 'та пока не понятно', likeCount: 34 },
+                { id: 0, message: 'учим react', likeCount: 1 },
+                { id: 0, message: 'тра-та-та', likeCount: 74 },
+            ],
+            newPostText: 'введите текст',
+        },
+
+        dialogsPage: {
+            dialogsData: [
+                { id: 0, name: "Petya" },
+                { id: 1, name: "Mary" },
+                { id: 2, name: "Sveta" },
+                { id: 3, name: "Nikaa" },
+                { id: 4, name: "Vasya" },
+                { id: 5, name: "Ivan" }
+            ],
+            messageData: [
+                { id: 0, message: "привет. че каго??" },
+                { id: 1, message: "Вроде пока чсто не понятно, но врооде потитхоньку" },
+                { id: 2, message: "ништяк))) ништяк))" },
+                { id: 3, message: "да-да-да" },
+                { id: 4, message: "посмотрим дельше" }
+            ]
+        }
+    },
+    getstate(){
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('!!!');
     },
 
-    dialogsPage: {
-        dialogsData: [
-            { id: 0, name: "Petya" },
-            { id: 1, name: "Mary" },
-            { id: 2, name: "Sveta" },
-            { id: 3, name: "Nikaa" },
-            { id: 4, name: "Vasya" },
-            { id: 5, name: "Ivan" }
-        ],
-        messageData: [
-            { id: 0, message: "привет. че каго??" },
-            { id: 1, message: "Вроде пока чсто не понятно, но врооде потитхоньку" },
-            { id: 2, message: "ништяк))) ништяк))" },
-            { id: 3, message: "да-да-да" },
-            { id: 4, message: "посмотрим дельше" }
-        ]
+    addPost() {
+        debugger;
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likeCount: 0
+        }
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
-
-}
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 0
-    }
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscrbe = (observer) => {
-    rerenderEntireTree = observer;
 }
 
 
-export default state;
+
+export default store;
+window.state = store;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import * as serviceWorker from './serviceWorker';
-import state, { subscrbe } from './Redux/state'
+import store from './Redux/state'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { addPost, updateNewPostText} from './Redux/state'
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -12,16 +11,17 @@ let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
-        <App state={state} addPost={addPost}  updateNewPostText={updateNewPostText}/>
+        <App state={state}  addPost={store.addPost.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)} />
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getstate());
 
-subscrbe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
