e4efc33039572b762667ca70efdf2f4987050c45
Ilya Shved <ilia83ivanovich@gmail.com>
2020-12-17 15:05:15 +0500

    53 classComponents

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,6 +3,52 @@ import React from 'react';
 import ss from './Users.module.css'
 import userPhoto from '../../assets/images/user.png'
 
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        Axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+
+    render() {
+        return (
+            <div className={ss.head}>
+
+                {
+                    this.props.users.map(u => <div key={u.id}>
+                        <span>
+                            <div><img src={u.photos.small != null ? u.photos.small : userPhoto} /></div>
+                            <div>
+
+                                {u.followed ?
+                                    <button onClick={() => { this.props.unfollow(u.id) }} >UNFOLLOW</button> :
+                                    <button onClick={() => { this.props.follow(u.id) }} >FOLLOW</button>}
+                            </div>
+                        </span>
+                        <span>
+                            <span>
+                                <div>{u.name}</div>
+                                <div>{u.status}</div>
+                            </span>
+                            <span>
+                                <div>{"u.location.country"}</div>
+                                <div>{"u.location.city"}</div>
+                            </span>
+                        </span>
+                    </div>)
+                }
+            </div>
+        )
+    }
+}
+
+export default Users;
+
+
+
+/*
 const Users = (props) => {
     let getUser = () => {
         if (props.users.length === 0) {
@@ -10,10 +56,8 @@ const Users = (props) => {
                 .then(response => {
                     props.setUsers(response.data.items);
                 });
-
         }
     }
-
 
     return (
         <div className={ss.head}>
@@ -44,29 +88,4 @@ const Users = (props) => {
         </div>
     )
 }
-
-export default Users;
-
-/*
-[
-    {
-        id: 1, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW2n09cekDvr7CTVplxhcBusDtvOnebiDmFg&usqp=CAU',
-        followed: false, fullName: "Vasya", status: "A im a boss", location: { city: 'Perm', country: "Russia" }
-    },
-    {
-        id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW2n09cekDvr7CTVplxhcBusDtvOnebiDmFg&usqp=CAU',
-        followed: true, fullName: "Petr", status: "A im a boss", location: { city: 'Perm', country: "Russia" }
-    },
-    {
-        id: 3, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW2n09cekDvr7CTVplxhcBusDtvOnebiDmFg&usqp=CAU',
-        followed: false, fullName: "Mary", status: "A im a boss", location: { city: 'Kiyv', country: "Ukrain" }
-    },
-    {
-        id: 4, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW2n09cekDvr7CTVplxhcBusDtvOnebiDmFg&usqp=CAU',
-        followed: true, fullName: "Joan", status: "A im a boss", location: { city: 'Minsk', country: "Belarus" }
-    },
-    {
-        id: 5, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW2n09cekDvr7CTVplxhcBusDtvOnebiDmFg&usqp=CAU',
-        followed: false, fullName: "Michel", status: "A im a boss", location: { city: 'Moscow', country: "Russia" }
-    },
-]*/
+*/
