a170a0846e1a51d26c40994270aff8e9976dbaf3
Ilya Shved <ilia83ivanovich@gmail.com>
2020-12-17 16:37:11 +0500

    54 componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,8 +4,7 @@ import ss from './Users.module.css'
 import userPhoto from '../../assets/images/user.png'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         Axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items);
